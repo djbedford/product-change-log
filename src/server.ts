9da71b2c9323c21c88c0c31ce73f1683d51f5900
David Bedford <beds2005@gmail.com>
2023-04-27 02:37:09 +0100

    Create API routes with logging

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import router from "./router";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("Hello from Express!");
   res.status(200);
   res.json({ message: "hello" });
 });
+
+app.use("/api", router);
 
 export default app;
